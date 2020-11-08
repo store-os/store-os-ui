f522e546558a3001c2ef92cfb91cd3da9077371f
Alex Albuerne <aalbuerneare@gmail.com>
2020-11-08 22:26:48 +0100

    Setting up initial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Typography } from 'antd';
+
+const { Title } = Typography;
+
+function App() {
+  return (
+    <div className="App">
+      <Title level={2}>👽 Some magic is missing here!</Title>
+    </div>
+  );
+}
+
+export default App;
