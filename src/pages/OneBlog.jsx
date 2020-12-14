b025ea015a17fdf80720891f749084626229c8e1
Alex Albuerne <aalbuerneare@gmail.com>
2020-12-14 22:11:00 +0100

    Refactoring blog detail page

--- src/pages/OneBlog.jsx
+++ src/pages/OneBlog.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 import {
   Avatar,
   Carousel,
-  Comment, 
+  Comment,
   Tooltip,
   Row,
   Card,
@@ -15,16 +15,16 @@ import {
   Tag,
 } from "antd";
 import {
-  LeftCircleFilled,
-  RightCircleFilled,
+  ArrowLeftOutlined,
+  ArrowRightOutlined,
   TwitterOutlined,
   YoutubeFilled,
   InstagramFilled,
   FacebookFilled,
   LinkedinFilled,
-} from '@ant-design/icons';
+} from "@ant-design/icons";
 
-const { utcToZonedTime, format } = require('date-fns-tz')
+const { utcToZonedTime, format } = require("date-fns-tz");
 const { Title, Paragraph } = Typography;
 const { Panel } = Collapse;
 
@@ -33,32 +33,24 @@ const OneBlog = () => {
   const [data, setData] = useState();
   const [selectedImage, setSelectedImage] = useState();
   const [previousPage, setPreviousPage] = useState();
-  
+
   const [nextPage, setNextPage] = useState();
   useEffect(async () => {
     const fetchData = async () => {
-      const result = await axios(
-        `${process.env.REACT_APP_BLOG_URL}/${blogId}`
-      );
+      const result = await axios(`${process.env.REACT_APP_BLOG_URL}/${blogId}`);
       setData(result.data);
       setSelectedImage(result.data.images[0]);
-      var prev = parseInt(result.data.id) - 1
-      if (prev !== 0){
-        setPreviousPage(String(prev))
+      var prev = parseInt(result.data.id) - 1;
+      if (prev !== 0) {
+        setPreviousPage(String(prev));
       }
-      var next = parseInt(result.data.id) + 1
+      var next = parseInt(result.data.id) + 1;
 
-      const limitPage = await axios(
-        `${process.env.REACT_APP_BLOG_URL}`
-      );
-      
-    
-      if (next <= limitPage.data.hits){
-        setNextPage(String(next))
+      const limitPage = await axios(`${process.env.REACT_APP_BLOG_URL}`);
+
+      if (next <= limitPage.data.hits) {
+        setNextPage(String(next));
       }
-    
-      
-      
     };
 
     fetchData();
@@ -67,131 +59,205 @@ const OneBlog = () => {
   return (
     <Main>
       {data && (
-        <Card>
-
-        {data.images && 
-          <Row>
-              <Carousel>
-                {data.images.map((image) => (
-                  <img src={image} alt=""></img>
-                ))}
-              </Carousel>
-          </Row>
-        }
-
         <Row>
-  
-            <Title level={1} style={{ marginTop: "2%" }}>
-              {data.title}
-            </Title>
-            
-            <Paragraph>
-              {data.author && 
-                <Comment
-                  author={data.author.name}
-                  avatar={
-                    <Avatar
-                      src={data.author.avatar}
-                      alt={data.author.name}
+          <Col span={6}>
+            <Back href={`/blog`}>
+              <ArrowLeftOutlined style={{ marginRight: 12 }} />
+              <span>Volver al blog</span>
+            </Back>
+          </Col>
+          <Col span={12}>
+            {data.images && (
+              <Row>
+                <BlogCarousel src={data.images[0]} alt="">
+                  {/* {data.images.map((image) => (
+                    <img src={image} alt=""></img>
+                  ))} */}
+                </BlogCarousel>
+              </Row>
+            )}
+
+            <Row>
+              <Title style={{ marginTop: "4%" }}>{data.title}</Title>
+              <Paragraph>
+                <PostInfo>
+                  {data.author && (
+                    <Comment
+                      author={data.author.name}
+                      avatar={
+                        <Avatar
+                          src={data.author.avatar}
+                          alt={data.author.name}
+                        />
+                      }
+                      content={<p>{data.author.role}</p>}
+                      datetime={
+                        data.date && (
+                          <Tooltip
+                            title={format(
+                              utcToZonedTime(
+                                new Date(data.date),
+                                "Europe/Madrid"
+                              ),
+                              "EEEE, dd/MM/yyyy",
+                              { timeZone: "Europe/Madrid" }
+                            )}
+                          >
+                            <span>
+                              {format(
+                                utcToZonedTime(
+                                  new Date(data.date),
+                                  "Europe/Madrid"
+                                ),
+                                "EEEE, dd/MM/yyyy",
+                                { timeZone: "Europe/Madrid" }
+                              )}
+                            </span>
+                          </Tooltip>
+                        )
+                      }
                     />
-                  }
-                  content={
-                    <p>
-                      {data.author.role}
-                    </p>
-                  }
-                  datetime={ data.date &&
-                    <Tooltip title={format(utcToZonedTime(new Date(data.date),'Europe/Madrid'),'EEEE, dd/MM/yyyy',{ timeZone: 'Europe/Madrid' })}>
-                      <span>{format(utcToZonedTime(new Date(data.date),'Europe/Madrid'),'EEEE, dd/MM/yyyy',{ timeZone: 'Europe/Madrid' })}</span>
-                    </Tooltip>
-                  }
-                />
-              }
-              <div dangerouslySetInnerHTML={{__html:data.content}}></div> 
+                  )}
+                  <TagContainer>
+                    {data.label.map((tag) => (
+                      <Tag>{tag}</Tag>
+                    ))}
+                  </TagContainer>
+                </PostInfo>
 
-            </Paragraph>
+                <HTMLContianer
+                  dangerouslySetInnerHTML={{ __html: data.content }}
+                ></HTMLContianer>
+              </Paragraph>
+            </Row>
+            <Row>
+              <Col span={12}>
+                {previousPage && (
+                  <Left href={`/blog/${previousPage}`}>
+                    <ArrowLeftOutlined style={{ marginRight: 12 }} />
+                    <span>Blog previo</span>
+                  </Left>
+                )}
+              </Col>
 
-            {data.label.map((tag) => (
-              <Tag>{tag}</Tag>
-              ))
-            }
+              <Col span={12}>
+                {nextPage && (
+                  <Right href={`/blog/${nextPage}`}>
+                    <span>Siguiente blog</span>
+                    <ArrowRightOutlined style={{ marginLeft: 12 }} />
+                  </Right>
+                )}
+              </Col>
+            </Row>
+          </Col>
+          <Col span={6}>
+            <Social>
+              {data.social.instagram && (
+                <a
+                  href={data.social.instagram}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <InstagramFilled style={{ backgroundColor: "#C13584" }} />
+                </a>
+              )}
 
-            {data.social.instagram &&
-              <a href={data.social.instagram} target="_blank">
-                <Tag icon={<InstagramFilled />} color="#C13584">
-                </Tag>
-            </a>
-            }
+              {data.social.facebook && (
+                <a href={data.social.facebook} target="_blank" rel="noreferrer">
+                  <FacebookFilled style={{ backgroundColor: "#3b5999" }} />
+                </a>
+              )}
 
-            {data.social.facebook &&
-              <a href={data.social.facebook} target="_blank">
-                <Tag icon={<FacebookFilled />} color="#3b5999">
-                </Tag>
-             </a>
-            }
+              {data.social.linkedin && (
+                <a href={data.social.linkedin} target="_blank" rel="noreferrer">
+                  <LinkedinFilled style={{ backgroundColor: "#55acee" }} />
+                </a>
+              )}
 
-            {data.social.linkedin &&
-              <a href={data.social.linkedin} target="_blank">
-                <Tag icon={<LinkedinFilled />} color="#55acee">
-                </Tag>
-            </a>
-            }
+              {data.social.twitter && (
+                <a href={data.social.twitter} target="_blank" rel="noreferrer">
+                  <TwitterOutlined style={{ backgroundColor: "#55acee" }} />
+                </a>
+              )}
 
-            {data.social.twitter &&
-              <a href={data.social.twitter} target="_blank">
-                <Tag icon={<TwitterOutlined />} color="#55acee">
-                </Tag>
-            </a>
-            }
-
-            {data.social.youtube &&
-              <a href={data.social.youtube} target="_blank">
-                <Tag icon={<YoutubeFilled />} color="#55acee">
-                </Tag>
-            </a>
-            }
+              {data.social.youtube && (
+                <a href={data.social.youtube} target="_blank" rel="noreferrer">
+                  <YoutubeFilled style={{ backgroundColor: "#55acee" }} />
+                </a>
+              )}
+            </Social>
+          </Col>
         </Row>
-        </Card>
       )}
-      <Row gutter={[16,16]}>
-      {previousPage && 
-      <Col span={12}>
-        <Left href={`/blog/${previousPage}`}> 
-        <Tag icon={<LeftCircleFilled />} >
-          </Tag>
-          Blog previo
-      </Left>
-      </Col>
-      }
-
-
-      { nextPage && 
-      <Col span={12}>
-      <Right href={`/blog/${nextPage}`}> 
-                Siguiente blog
-                <Tag icon={<RightCircleFilled />} >
-                </Tag>
-            </Right>
-      </Col>
-      }
-      </Row>
     </Main>
-    
   );
 };
 
 const Main = styled.main`
-  padding: 4%;
+  margin-bottom: 80px;
 `;
 
+const BlogCarousel = styled.img`
+  width: 100%;
+  height: 300px;
+  object-fit: cover;
+  object-position: top;
+`;
+
+const HTMLContianer = styled.div`
+  & img {
+    width: 100%;
+  }
+`;
+
+const PostInfo = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+`;
+
+const Social = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+margin: 40px 0;
+}
+& > a {
+  margin-bottom: 18px;
+}
+  & > a > span {
+    width: 42px;
+    height: 42px;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    color: white;
+    border-radius: 50%;
+    & > svg {
+      font-size: 20px;
+    }
+  }
+`;
+
+const TagContainer = styled.div``;
+
 const Left = styled.a`
-  float:left;
-  width:100px;
+  display: flex;
+  align-items: center;
 `;
 
 const Right = styled.a`
-  float:right;
-  width:100px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`;
+
+const Back = styled.a`
+  display: inline-flex;
+  width: calc(100% - 120px);
+  justify-content: flex-end;
+  align-items: center;
+  margin: 20px 60px;
 `;
 
 export default OneBlog;
